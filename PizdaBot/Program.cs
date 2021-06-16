c6ac878fbc283a0aa7c9bd961aea5c4226d5d851
thepunkoff <thepunkoff@gmail.com>
2021-06-16 15:04:22 +0300

    update

--- PizdaBot/Program.cs
+++ PizdaBot/Program.cs
@@ -10,17 +10,37 @@ namespace PizdaBot
     {
         static async Task Main(string[] args)
         {
-            var token = await System.IO.File.ReadAllTextAsync("token.txt");
-            var client = new TelegramBotClient(token);
+            TelegramBotClient client = null;
+            try
+            {
+                var token = await System.IO.File.ReadAllTextAsync("token.txt");
+                token = token.Replace("\r", string.Empty);
+                token = token.Replace("\n", string.Empty);
+                Console.WriteLine(token);
+                client = new TelegramBotClient(token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
             client.OnMessage += (_, ea) =>
             {
+                Console.WriteLine($"New message: {ea.Message.Text ?? "null"}");
                 try
                 {
                     if (ea.Message.Text.ToLowerInvariant() == "да")
+                    {
+                        Console.WriteLine("Sending kirkorov");
                         client.SendStickerAsync(ea.Message.Chat, new InputMedia("CAACAgIAAxkBAAMsYMndSbtmS8jFrKeWzcpHj8PCHDwAAgIAA8rM2SfczZz_qtrYGh8E"));
+                    }
 
                     if (ea.Message.Text.ToLowerInvariant() == "нет")
+                    {
+                        Console.WriteLine("Sending blowjob");
                         client.SendTextMessageAsync(ea.Message.Chat, "Минет.");
+                    }
                 }
                 catch (Exception ex)
                 {
